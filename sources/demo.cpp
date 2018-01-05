1611d012bb393d8ea3c38db9f4df353ed306b4b0
babyrage666 <kapotyuk0901@yandex.ru>
2018-01-06 01:33:39 +0300

    Update demo.cpp

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -6,83 +6,76 @@
 
 using namespace boost::filesystem;
 using namespace boost::program_options;
+std::string text;
 
 int main(int argc, char** argv) 
 {
-    try 
-    {
-        std::string text;
-
-        options_description desc{"Options"};
-        desc.add_options()
-        ("help,h", "Help screen")
-        ("output", value<std::string>()/*->notifier(out_func)*/, "Output");
-
-        variables_map vm_console;
-        variables_map vm_file;
+    try
+    {    
+        options_description options_desc{"General options"}; 
+        options_desc.add_options()
+        ("help,h", "Show help")
+        ("output,o", value<std::string>(), "Output p");
         
-        store(parse_command_line(argc, argv, desc), vm_console);
-        notify(vm_console);
-    
-        std::string gAddress = getenv("HOME");
-        gAddress += "/.config/demo.cfg";
-        const char* _gAddress = gAddress.c_str();
-
-        if(exists(gAddress))
-            store(parse_config_file<char>( _gAddress, desc ), vm_file);
+        variables_map console;
+        store(parse_command_line(argc, argv, options_desc), console);
+        notify(console);
         
-        notify(vm_file);
+        if(!getenv("HOME")){
+            std::cout << "HOME not found - error" << std::endl;
+            return 0;
+        }
+           
+        std::string home_path = getenv("HOME");
+        home_path += "/.config/demo.cfg";
+
+        variables_map file;
+        
+        if(boost::filesystem::exists(home_path)) {
+            store(parse_config_file<char>( home_path.c_str(), options_desc), file);
+        }
+        
+        notify(file);
     
-        if (vm_console.count("help") || vm_file.count("help")) 
-        {
-            std::cout << desc << '\n';
-        } 
-        else if(vm_console.count("output")) 
-        {
-            std::cout << "Output in >> " << vm_console["output"].as<std::string>() << '\n';
-            std::ofstream out(vm_console["output"].as<std::string>(), std::ios_base::app);
-            while(std::cin >> text) 
-            {
-                print(text, out);
-                out << std::endl;
-            }
-            out.close();
-        } 
-        else if (getenv("DEMO_OUTPUT") != nullptr) 
-        {
-            std::cout << "ENV" << std::endl;
-            std::string _trAddress = getenv("DEMO_OUTPUT");
-            std::ofstream out(_trAddress, std::ios_base::app);
-            while(std::cin >> text) 
-            {
-                print(text, out);
-                out << std::endl;
-            }
-            out.close();
-        } 
-        else if (vm_file.count("output")) 
-        {
-            std::cout << "Output in >> " << vm_file["output"].as<std::string>() << '\n';
-            std::ofstream out(vm_file["output"].as<std::string>(), std::ios_base::app);
-            while(std::cin >> text) 
-            {
-                print(text, out);
-                out << std::endl;
-            }
-            out.close();
-        } 
-        else 
-        {
-            std::cout << "DEFAULT" << std::endl;
-            std::ofstream out("default.log", std::ios_base::app);
-            while(std::cin >> text) 
-            {
+        if(console.count("help") || file.count("help")) {
+            std::cout << options_desc << std::endl;
+        }
+        else if(console.count("output")) {
+            std::ofstream out(console["output"].as<std::string>(), std::ios_base::app);
+            while(std::cin >> text) {
                 print(text, out);
                 out << std::endl;
             }
             out.close();
         }
-
+        else if (getenv("DEMO_OUTPUT")) {
+            std::cout << "env" << std::endl;
+            std::string _trAdd = getenv("DEMO_OUTPUT");
+            std::ofstream out(_trAdd, std::ios_base::app);
+            while(std::cin >> text) {
+                print(text, out);
+                out << std::endl;
+            }
+            out.close();
+        }
+        else if (file.count("output")) {
+            std::cout << "Output in >> " << file["output"].as<std::string>() << std::endl;
+            std::ofstream out(file["output"].as<std::string>(), std::ios_base::app);
+            while(std::cin >> text) {
+                print(text, out);
+                out << std::endl;
+            }
+            out.close();
+        }
+        else {
+            std::cout << "DEFAULT" << std::endl;
+            std::ofstream out("default.log", std::ios_base::app);
+            while(std::cin >> text) {
+                print(text, out);
+                out << std::endl;
+            }
+            out.close();
+        }
     }
 
     catch (const error &ex) 
